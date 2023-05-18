dd6e089d9eb317d8f319d2360b0e2f9a1401fdb9
Mathieu Marleix <git@aerian.fr>
2023-05-18 18:38:38 +0200

    feat: add current details

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-video_folder=visualization/videos/howtovalidation
-output_folder=visualization/output/howtovalidation
-pdvc_model_path=save/howto100m-tsp_anet_tsp_pdvc_v_2023-05-18-18-10-37/model-best.pth
+video_folder=visualization/videos/original
+output_folder=visualization/output/original
+pdvc_model_path=save/anet_tsp_pdvc/model-best.pth
 output_language=en
 sh test_and_visualize.sh $video_folder $output_folder $pdvc_model_path $output_language
